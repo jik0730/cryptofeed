db994fcd94cbb427399b4821e2ce5a717f1bb4cd
Ingyo Chung <jik0730@gmail.com>
2021-12-16 10:41:14 +0900

    test: test okex liq failure situation due to api rate limit

--- cryptofeed/exchange/okex.py
+++ cryptofeed/exchange/okex.py
@@ -41,6 +41,7 @@ class OKEx(Feed):
     def __init__(self, **kwargs):
         super().__init__('wss://ws.okex.com:8443/ws/v5/public', **kwargs)
         self.api_max_try = 10
+        self.liq_rate_limit = 1  # 2 req per 2 seconds
 
     async def _trade(self, msg: dict, timestamp: float):
         """
@@ -84,50 +85,52 @@ class OKEx(Feed):
                 else:
                     continue
 
-                for status in (FILLED, UNFILLED):
-                    end_point = f"{self.api}v5/public/liquidation-orders?instType={instrument_type}&limit=100&state={status}&uly={uly}"
-
-                    shortage_flag = True
-                    entries = []
-                    for retry in range(self.api_max_try):
-                        _end_point = end_point if len(entries) == 0 else end_point + '&after=' + str(data['data'][0]['details'][-1]['ts'])
+                end_point = f"{self.api}v5/public/liquidation-orders?instType={instrument_type}&limit=100&state=filled&uly={uly}"
+                shortage_flag = True
+                entries = []
+                for retry in range(self.api_max_try):
+                    _end_point = end_point if len(entries) == 0 else end_point + '&after=' + str(data['data'][0]['details'][-1]['ts'])
+                    try:
                         data = await self.http_conn.read(_end_point)
-                        data = json.loads(data, parse_float=Decimal)
-                        timestamp = time.time()
+                    except Exception as e:
+                        LOG.warning("%s: Failed to get liquidations with possible data shortage: %s", self.id, e)
+                        break
+                    data = json.loads(data, parse_float=Decimal)
+                    timestamp = time.time()
 
-                        if len(data['data'][0]['details']) == 0:
-                            break
-                        
-                        for entry in data['data'][0]['details']:
-                            if pair in last_update:
-                                if float(entry['ts']) <= float(last_update[pair].get(status)['ts']):
-                                    shortage_flag = False
-                                    break
-                            else:
+                    if len(data['data'][0]['details']) == 0:
+                        break
+                    
+                    for entry in data['data'][0]['details']:
+                        if pair in last_update:
+                            if float(entry['ts']) <= float(last_update.get(pair)['ts']):
                                 shortage_flag = False
-                            entries.append(entry)
+                                break
+                        else:
+                            shortage_flag = False
+                        entries.append(entry)
 
-                        if retry == 0:  # store latest data
-                            last_update[pair][status] = data['data'][0]['details'][0]
-                        await asyncio.sleep(0.1)
-
-                        if not shortage_flag:  # break if no new data
-                            break
-
-                        if shortage_flag and retry == self.api_max_try - 1:  # notify if number of retries is not enough for data shortage
-                            LOG.warning("%s: Possible %s data shortage", self.id, LIQUIDATIONS)
-                        
-                    for entry in entries[::-1]:  # insert oldest first
-                        await self.callback(LIQUIDATIONS,
-                                            feed=self.id,
-                                            symbol=pair,
-                                            side=BUY if entry['side'] == 'buy' else SELL,
-                                            leaves_qty=Decimal(entry["sz"]),
-                                            price=Decimal(entry["bkPx"]),
-                                            order_id=None,
-                                            timestamp=timestamp_normalize(self.id, float(entry["ts"])),
-                                            receipt_timestamp=timestamp)
+                    if retry == 0:  # store latest data
+                        last_update[pair] = data['data'][0]['details'][0]
                     await asyncio.sleep(0.1)
+
+                    if not shortage_flag:  # break if no new data
+                        break
+
+                    if shortage_flag and retry == self.api_max_try - 1:  # notify if number of retries is not enough for data shortage
+                        LOG.warning("%s: Possible %s data shortage", self.id, LIQUIDATIONS)
+                    
+                for entry in entries[::-1]:  # insert oldest first
+                    await self.callback(LIQUIDATIONS,
+                                        feed=self.id,
+                                        symbol=pair,
+                                        side=BUY if entry['side'] == 'buy' else SELL,
+                                        leaves_qty=Decimal(entry["sz"]),
+                                        price=Decimal(entry["bkPx"]),
+                                        order_id=None,
+                                        timestamp=timestamp_normalize(self.id, float(entry["ts"])),
+                                        receipt_timestamp=timestamp)
+                await asyncio.sleep(0.1)
             
             time_to_sleep = round(60 - time.time() % 60, 6)  # call every minute
             await asyncio.sleep(time_to_sleep)
